cab212b93c4872f1938d2cf9cae6c36f20a58b87
Wipying Jin <wipying@hotmail.com>
2014-05-21 10:01:22 +0800

    Add a helper to application

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title){"Ruby on Rails Tutorial Sample App"}
   describe "Home Page" do
   	it "should have the content 'Sample App'" do
   		visit '/static_pages/home'
@@ -8,8 +9,9 @@ describe "StaticPages" do
   	end
     it "should have the title with the content 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
+    
   end
 
   describe "Help Page" do
@@ -19,7 +21,7 @@ describe "StaticPages" do
   	end
     it "should have the title with the content 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -30,9 +32,22 @@ describe "StaticPages" do
   	end
     it "should have the title with the content 'About'" do
       visit '/static_pages/about'
-      expect(page).to have_title('About')
+      expect(page).to have_title("#{base_title} | About")
     end
   end
+
+  describe "Contact" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    it "should have the title with the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+  end
+
+
 
 
 end
